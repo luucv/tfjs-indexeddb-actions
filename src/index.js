bd5ae7dffeae256d9df7a784b2e2dbce2fc3b8c3
Luuc Verburgh <luuc.verburgh@gmail.com>
2020-02-03 10:30:30 +0100

    main: adds entry interface and tests

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import load from './load';
 import store from './store';
 
 export default {
-  load,
-  store,
+  async loadAndStoreLayersModel(url, name) {
+    const artifacts = await store.convertUrlToArtifacts(url);
+    await store.storeAction(artifacts, name);
+    const model = await load.loadAction(name);
+
+    return model;
+  },
+  async loadFromIndexedDb(name) {
+    const model = await load.loadAction(name);
+
+    return model;
+  },
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
 import '@babel/polyfill'
 import * as tf from '@tensorflow/tfjs';
 
+import indexedDbService from '../src/index';
 import load from '../src/load';
 import store from '../src/store';
 import utils from '../src/utils/utils';
@@ -8,6 +9,7 @@ import { artifacts } from './mocks/model';
 import Upsampling from './mocks/Upsampling';
 
 const BIG_MODEL_URL = 'http://localhost:5000/static/models/dextr/model.json';
+const IRIS_MODEL = 'https://storage.googleapis.com/tfjs-models/tfjs/iris_v1/model.json';
 
 describe('functional', async () => {
   beforeEach(async function() {
@@ -35,5 +37,18 @@ describe('functional', async () => {
 
     expect(model).not.toEqual(null);
     expect(model.layers.length).toEqual(372);
+  });
+
+  it('loads and stores a model', async () => {
+    const model = await indexedDbService.loadAndStoreLayersModel(IRIS_MODEL, 'iris');
+    expect(model).not.toEqual(null);
+  });
+
+  it('loads from indexedDB', async () => {
+    const model = await indexedDbService.loadAndStoreLayersModel(IRIS_MODEL, 'iris');
+    const idbModel = await indexedDbService.loadFromIndexedDb('iris');
+
+    expect(idbModel).not.toEqual(null);
+    expect(idbModel.name).toEqual(model.name);
   });
 });
