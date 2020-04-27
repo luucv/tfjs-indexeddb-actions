4fec4349632eb20907d22d050664054965b73f53
Luuc Verburgh <luuc.verburgh@gmail.com>
2020-04-27 21:26:18 +0200

    feat(db): match tests

--- test/chunckeddb.test.js
+++ test/chunckeddb.test.js
@@ -1,7 +1,7 @@
 import '@babel/polyfill'
 import * as tf from '@tensorflow/tfjs';
 
-import { indexedDBRouter } from '../src/chunked_indexeddb.js';
+import { chunkedDBRouter } from '../src/chunkeddb.js';
 import load from '../src/load';
 import utils from '../src/utils/utils';
 import { artifacts } from './mocks/model';
@@ -17,19 +17,24 @@ describe('ChunckedIndexedDB', async () => {
   });
 
   it('returns indexedDB retreiver based on url path', async () => {
-    const handler = indexedDBRouter('chunked-indexeddb://foo');
-    expect(handler.URL_SCHEME).to.equal('chunked-indexeddb://');
+    const handler = chunkedDBRouter('chunkeddb://foo');
+    expect(handler.modelPath).toEqual('foo');
+  });
+
+  it('returns null if path not chunkeddb://', async () => {
+    const modelHandler = chunkedDBRouter('foo');
+    expect(modelHandler).toEqual(null);
   });
 
   it('roundtrip small model', async () => {
     tf.serialization.registerClass(Upsampling);
 
-    const modelHandler = indexedDBRouter('chunked-indexeddb://foo');
+    const modelHandler = chunkedDBRouter('chunkeddb://foo');
     await modelHandler.save(artifacts);
 
     const modelArtifacts = await modelHandler.load();
     const model = await load.convertModelArtifactsToModel(modelArtifacts);
-
+    
     expect(model.name).toEqual('sequential_1');
   });
 });
